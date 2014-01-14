433aa763fa98435677885df03d76b4af912afa5e
Zeph Lafassett <zeph@lafassett.com>
2014-01-14 09:10:10 -0800

    Adding comment to head of file

--- examples/00_test/lib/stringy.js
+++ examples/00_test/lib/stringy.js
@@ -1,3 +1,9 @@
+/**
+ * Combined version for simple example
+ * 
+ * Appends an exclamation mark (!) at the end of a string
+ * 
+ */
 module.exports = function stringy (input) {
 	return input + "!";
 };
