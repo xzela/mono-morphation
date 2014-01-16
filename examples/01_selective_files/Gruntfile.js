447e2f73a30142fae0e4aa29ba97ca341d0cf821
Zeph Lafassett <zeph@lafassett.com>
2014-01-15 18:24:04 -0800

    Adding grunt file and updating package.json file to include grunt tasks

---
+++ examples/01_selective_files/Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+        browserify: {
+            main: {
+                options: {
+                    debug: true
+                }
+            },
+            files: {
+                'bundle.js': 'index.js'
+            }
+        }
+	});
+    //tell Grunt to load taks from NPM
+    grunt.loadNpmTasks('grunt-browserify');
+
+    // add tasks
+    grunt.registerTask('default', ['browserify']);
+};
