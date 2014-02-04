b5023df2bab1736cb5bebc62b4c374ce55328813
Zeph Lafassett <zeph@lafassett.com>
2014-02-04 09:39:54 -0800

    Adding initial html files and static views

---
+++ examples/02_form_validation/index.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+	app = express(),
+	path = require('path'),
+	http = require('http');
+
+app.configure(function () {
+	app.set('port', 3000);
+	app.set('views', __dirname + '/public/views');
+	app.use(express.static(__dirname + '/public'));
+	app.engine('html', require('ejs').renderFile);
+});
+
+app.get('/', function (req, res) {
+	res.render('index.html');
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('server created and listening on: ' + app.get('port'));
+});
+//
